4c4eb20c3a71d4a0975c37960cde829fa8677da3
Rishabh Rao <rishabhsrao@gmail.com>
2019-07-24 12:45:36 +0200

    Since 'testing' is specific to this repository, just use a post build step to compile it

--- build/build-flow.sh
+++ build/build-flow.sh
@@ -2,4 +2,3 @@
 set -eoux pipefail
 
 ./node_modules/.bin/flow-copy-source --verbose src lib
-[ -d "testing" ] && ./node_modules/.bin/flow-copy-source --verbose testing lib/testing
--- build/build-lib.sh
+++ build/build-lib.sh
@@ -2,4 +2,3 @@
 set -eoux pipefail
 
 ./node_modules/.bin/babel --out-dir lib src
-[ -d "testing" ] && ./node_modules/.bin/babel --out-dir lib/testing testing
