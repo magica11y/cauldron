77a6d8e5108521028b906ca26a60f30bc1a19209
Rishabh Rao <rishabh@yelp.com>
2020-10-16 10:01:20 +0200

    Fix lint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,15 +4,8 @@ module.exports = {
     es6: true,
     jest: true,
   },
-  plugins: [
-    'flowtype',
-  ],
-  extends: [
-    'airbnb-base',
-    'plugin:jest/recommended',
-    'plugin:prettier/recommended',
-    'plugin:flowtype/recommended',
-  ],
+  plugins: ['flowtype'],
+  extends: ['airbnb-base', 'plugin:jest/recommended', 'plugin:prettier/recommended', 'plugin:flowtype/recommended'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
