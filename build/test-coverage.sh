5dfa9dbe81f0bcbd6ac6b15ecd44ad320aa6da46
Rishabh <342789+rishabh-ink@users.noreply.github.com>
2025-01-20 20:29:45 +0530

    Switch to coveralls-next and use COVERALLS_REPO_TOKEN

--- build/test-coverage.sh
+++ build/test-coverage.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-set -eoux pipefail
+set -eou pipefail
 
-yarn test --coverageReporters=text-lcov | COVERALLS_REPO_TOKEN=$COVERALLS_REPO_TOKEN ./node_modules/.bin/coveralls
+if [[ -z "${COVERALLS_REPO_TOKEN:-}" ]]; then
+  echo "Please set environment variable, COVERALLS_REPO_TOKEN, from https://coveralls.io/github/magica11y for this repository."
+else
+  yarn test --coverageReporters=lcov && COVERALLS_REPO_TOKEN=$COVERALLS_REPO_TOKEN ./node_modules/.bin/coveralls < coverage/lcov.info
+fi
