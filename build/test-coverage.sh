93ab7ba08d74d5f69ece012584f516cb411872e1
Rishabh Rao (GitHub) <rishabh@yelp.com>
2025-01-20 19:29:53 +0530

    Add repo token which is needed by Coveralls during upload

--- build/test-coverage.sh
+++ build/test-coverage.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -eoux pipefail
 
-yarn test --coverageReporters=text-lcov | ./node_modules/.bin/coveralls
+yarn test --coverageReporters=text-lcov | COVERALLS_REPO_TOKEN=$COVERALLS_REPO_TOKEN ./node_modules/.bin/coveralls
