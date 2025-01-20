a2282580675ee02812a25a9f4b08fde2d51a1d92
Rishabh <342789+rishabh-ink@users.noreply.github.com>
2025-01-20 21:20:08 +0530

    Exit with error code if COVERALLS_REPO_TOKEN is not set

--- build/test-coverage.sh
+++ build/test-coverage.sh
@@ -3,6 +3,7 @@ set -eou pipefail
 
 if [[ -z "${COVERALLS_REPO_TOKEN:-}" ]]; then
   echo "Please set environment variable, COVERALLS_REPO_TOKEN, from https://coveralls.io/github/magica11y for this repository."
+  exit 1
 else
   yarn test --coverageReporters=lcov && COVERALLS_REPO_TOKEN=$COVERALLS_REPO_TOKEN ./node_modules/.bin/coveralls < coverage/lcov.info
 fi
