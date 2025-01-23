bf297264804f16417210148d38be8e0b94fae4bf
Rishabh <342789+rishabh-ink@users.noreply.github.com>
2025-01-23 08:54:19 +0530

    Expect 100% code coverage

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,12 @@
 module.exports = {
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
